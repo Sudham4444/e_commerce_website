0499296449f944e9dba59fdda3cc4cad6a056fbd
sudham <singhsudham4444@gmail.com>
2024-09-05 21:46:06 +0530

    Prepare for Render deployment

--- e_commerce_website/settings.py
+++ e_commerce_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-152182xtjn+&%q9^xs580&*n8v808t*mnn@d5(ea#s0=r!w1y%'
+# SECRET_KEY = 'django-insecure-152182xtjn+&%q9^xs580&*n8v808t*mnn@d5(ea#s0=r!w1y%'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-152182xtjn+&%q9^xs580&*n8v808t*mnn@d5(ea#s0=r!w1y%')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
+# Render-specific settings
+ALLOWED_HOSTS = ['*']
+DEBUG = False
+
 
 
 # Application definition
@@ -123,6 +130,12 @@ LOGIN_REDIRECT_URL = '/profile/'
 # EMAIL_BACKEND = 'django.core.mail.backend.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 # Default primary key field type
